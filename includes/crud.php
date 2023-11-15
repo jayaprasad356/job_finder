5ad6a216afc481c5476da6487a3bcad1e0225efa
AKraja777 <ashokkumar7hitter@gmail.com>
2023-11-15 18:42:08 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -28,9 +28,9 @@ class Database
 
     //LIVE
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "job_finder";    // Change as required
+    private $db_user = "u743445510_job_finder";  // Change as required
+    private $db_pass = "Job_finders@2023";  // Change as required
+    private $db_name = "u743445510_job_finder";    // Change as required
 
 
     /*
--- public/login-form.php
+++ public/login-form.php
@@ -51,7 +51,7 @@ if (isset($_POST['btnLogin'])) {
     <!-- general form elements -->
     <div class='row'>
         <div class="col-md-12 text-center">
-            <img src="dist/img/" height="110">
+            <img src="dist/img/boxed-bg.png" height="110">
             <h3>Job Finder-Dashboard</h3>
         </div>
         <div class="box box-info col-md-12">
